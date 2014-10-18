98a79bff54ca8816793df885c3ecb6b7d6d0cc4e
Kunal0615 <kunal0615@gmail.com>
2014-10-18 00:05:33 -0700

    hipchat wait

--- test_code__VS_connect/test_code__VS_connect/Program.cs
+++ test_code__VS_connect/test_code__VS_connect/Program.cs
@@ -13,6 +13,7 @@ namespace test_code__VS_connect
             Console.WriteLine("Lets see if this gets commited to git hub :)");
             Console.WriteLine("this the second the change i am making to the file... and the second commit from the VS_13..DIRECTLY");
             Console.WriteLine("NOW on all the changes can be made directly .... no rework .... GOD bless LINUS");
+            Console.WriteLine("Lets see when these will be reflected on hipcaht");
         }
     }
 }
